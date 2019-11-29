4be7601673dcd2360d035c71295894bb43b7d9c0
Corentin Crébier <bibo5088@gmail.com>
2019-11-29 10:38:10 +0100

    reminders

--- build/index.js
+++ build/index.js
@@ -47,6 +47,33 @@ typeorm_1.createConnection().then(() => {
                 }
             }, 1000 * 30);
         }());
+        //Reminders
+        (async function reminders() {
+            const sendFeedbackReminder = async (channel) => {
+                await channel.send(`> This server is always looking for feedback on ways to improve. Suggestion are welcome either in #meta or as a DM to our staff. 
+> However, if you have a feedback, but you wish to stay anonymous, we have a form for that. This form is also listed in #rules 
+__Form:__
+<https://docs.google.com/forms/d/1n5zAsDeFgZWLpzDlbXvYPIPXxPS4t82Kcw1FIQKF7mk>
+__Current Feedback:__
+<https://docs.google.com/spreadsheets/d/140xA8Qiazq6rlITZXmL0omRXSnxk49oBHRadcFvVqyA/edit?usp=sharing>`);
+            };
+            const sendArtReminder = async (channel) => {
+                await channel.send("> Remember to credit the artist so people can find their artwork.");
+            };
+            //Load channels
+            const feedbackReminderChannels = JSON.parse(process.env.FEEDBACK_REMINDER_CHANNELS);
+            const artReminderChannels = JSON.parse(process.env.ART_REMINDER_CHANNELS);
+            //Feedback
+            for (const [channelId, interval] of feedbackReminderChannels) {
+                const channel = client.channels.find(c => c.id === channelId);
+                setInterval(sendFeedbackReminder.bind(null, channel), interval * 1000);
+            }
+            //Art
+            for (const [channelId, interval] of artReminderChannels) {
+                const channel = client.channels.find(c => c.id === channelId);
+                setInterval(sendArtReminder.bind(null, channel), interval * 1000);
+            }
+        })();
         console.log("Bot is ready");
     });
     client.on("message", async (msg) => {
